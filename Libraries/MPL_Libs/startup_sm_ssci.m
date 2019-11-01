3c866af24d2357473a32309e046447509cf8ddcf
Steve Miller <smiller@mathworks.com>
2019-11-01 18:10:14 +0100

    Initial commit R2018a

--- Libraries/MPL_Libs/startup_sm_ssci.m
+++ Libraries/MPL_Libs/startup_sm_ssci.m
@@ -10,6 +10,13 @@ if((exist('+forcesPS')==7) && ~exist('forcesPS_Lib'))
 end
 cd ..
 
+SCI_libname = 'Multibody_Multiphysics_Lib';
+load_system(SCI_libname);
+SPL_ver = get_param(SCI_libname,'Description');
+disp(SPL_ver);
+which(SCI_libname)
+
+
 if exist('Multibody_Multiphysics_Demo_Script.html')
     web('Multibody_Multiphysics_Demo_Script.html');
 end
