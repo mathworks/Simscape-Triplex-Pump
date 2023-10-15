3763621b09a0b15acd8ef23afb940b3e38baa7e7
Steve Miller <smiller@mathworks.com>
2023-10-15 12:31:45 +0200

    Fixed script that creates Data folder

--- Workflows/Fault_Sweep/sm_pump_triplex_DOEdata_tocsv.m
+++ Workflows/Fault_Sweep/sm_pump_triplex_DOEdata_tocsv.m
@@ -2,10 +2,12 @@ function sm_pump_triplex_DOEdata_tocsv(test_Data)
 % Code to generate measurement data with noise and missing measurements
 % from simulation results of sm_pump_triplex.slx
 %
-% Copyright 2017-2023 The MathWorks, Inc.
+% Copyright 2017-2022 The MathWorks, Inc.
 
 % Make data directory if it doesn't exist
-if ~exist('Data', 'dir')
+
+cd(fileparts(which(mfilename')));
+if ~(isfolder([pwd filesep 'Data']))
     mkdir('Data')
 end
 
